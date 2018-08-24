09a63b53e1823339f4ec1b968e61bc94db00ad0d
jvpink200 <jvillarr20@gmail.com>
2018-08-24 02:40:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
+function addToCart(itemName) {
+  var price = Math.floor(Math.random() * Math.floor(100));
+  cart.push({[itemName]:price});
  // write your code here
+
+ console.log(`${itemName} has been added to your cart`)
+ console.log(cart);
 }
 
 function viewCart() {
